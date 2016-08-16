b2113a1fc2f16214536714f38621573f000a6bec
tchgc <tiincho.alvarez@gmail.com>
2016-08-16 17:49:44 -0300

    subir imagen, sin validar nada..a wacha pelada

---
+++ client/template/profile/images/upload_images.js
@@ -0,0 +1,26 @@
+Template.uploadImages.helpers({
+	images: function () {
+		return Images.find(); // Where Images is an FS.Collection instance
+	}
+});
+
+	/*Template.uploadImages.events({
+		'change .myFileInput': function(event, template) {
+			var files = event.target.files;
+			for (var i = 0, ln = files.length; i < ln; i++) {
+				Images.insert(files[i], function (err, fileObj) {
+					// Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+				});
+			}
+		}
+
+	}); */
+
+
+	Template.uploadImages.events({
+    'click input[type="submit"]': function () {
+      var file = $('#file').get(0).files[0];
+      var result = Images.insert(file);
+      console.log('Upload result: ', result);
+    }
+  });
