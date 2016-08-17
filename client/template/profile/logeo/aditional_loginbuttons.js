4f2ca649f509a6c960451507b5420f0c143fe129
Federico Gomez <federico_135@hotmail.com>
2016-08-16 22:14:14 -0300

    se arreglaron errores corregidos x Franco, falta insertar en profile campos al registrarse y al editProfile

--- client/template/profile/logeo/aditional_loginbuttons.js
+++ client/template/profile/logeo/aditional_loginbuttons.js
@@ -1,7 +1,7 @@
 //Template que captura evento de click en editarPerfil y redirige al template editProfile
 Template._loginButtonsLoggedInDropdown.events({
 	'click #login-buttons-edit-profile': function(event) {
-		Router.go('editProfile');
+		Router.go('editProfile', {_id: Meteor.userId()});
 	}
 });
 //agregada para traduciar al castellano lo relacionado con el package accounts-ui
