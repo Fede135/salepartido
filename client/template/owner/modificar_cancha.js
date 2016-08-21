948683c3ff93726e96583b0fc88afbee90bd53b8
tchgc <tiincho.alvarez@gmail.com>
2016-08-21 03:07:15 -0300

    se agrego pantalla Gestionar Recintos, modificar canchas..Funciona todo ok

---
+++ client/template/owner/modificar_cancha.js
@@ -0,0 +1,25 @@
+Template.modificarCancha.helpers({
+	canchas: function () {
+		return Canchas.find();
+	},
+
+	cantJugadores: function(){
+		return ["5 vs 5","6 vs 6","7 vs 7","11 vs 11"].map((el) => ({label: el, value: el}));
+	},
+
+	tipoCancha : function(){
+		return ["Cesped Sintetico","Cesped Natural","Valdoza","Tierra"].map((al) => ({label: al, value: al}));
+	},
+
+	estadoCancha : function(){
+		return ["Habilitada","No Habilitada","Mantenimiento"].map((ol) => ({label: ol, value: ol}));
+	},
+
+	updateFormName: function () {
+            return "updateOrgForm-" + this._id;
+        },
+
+	countCanchas: function (){
+		return Canchas.find().count();
+	},
+});
