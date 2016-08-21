b3b5365bf0f03cc453ac120eba81ae8ff2eca3ec
tchgc <tiincho.alvarez@gmail.com>
2016-08-21 03:46:57 -0300

    Funcionalidad Gestionar Recinto terminada, se crea, modifica y elimina una cancha

---
+++ client/template/owner/eliminar_cancha.js
@@ -0,0 +1,20 @@
+Template.eliminarCancha.helpers({
+	canchas: function () {
+		return Canchas.find();
+	},
+
+	  onError: function () {
+      return function (error) { alert("BOO!"); console.log(error); };
+    },
+    onSuccess: function () {
+      return function (result) { alert("YAY!"); console.log(result); };
+    },
+    beforeRemove: function () {
+      return function (collection, id) {
+        var doc = Canchas.findOne(id);
+        if (confirm('Realmente quiere eliminar la cancha numero  "' + doc.numero + '"?')) {
+          this.remove();
+        }
+      };
+    }
+});
