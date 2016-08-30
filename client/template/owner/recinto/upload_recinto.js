f8f6ed6fba2fe7fb25580b272c7d12fa716d5168
Franco Catena <francocatena@gmail.com>
2016-08-30 12:02:04 -0300

    Cambio de paquete de subida de imágenes

--- client/template/owner/recinto/upload_recinto.js
+++ client/template/owner/recinto/upload_recinto.js
@@ -1,28 +1,26 @@
-/*
-Segun la documentacion del paquete
-AutoForm.addHooks(
-  ["uploadRecinto"],
-  {
-    before   : {
-      method: CfsAutoForm.Hooks.beforeInsert
-    },
-    after    : {
-      method: CfsAutoForm.Hooks.afterInsert
-    }
-  }
-);
+// AutoForm.addHooks(
+//   ["uploadRecinto"],
+//   {
+//     before   : {
+//       method: CfsAutoForm.Hooks.beforeInsert
+//     },
+//     after    : {
+//       method: CfsAutoForm.Hooks.afterInsert
+//     }
+//   }
+// );
 
-AutoForm.hooks({
- uploadRecinto: {
-   after: {
-     insert: function(error, result, template) {
-       insertedFile = Recintos.findOne(result).imagenes_id;
-       Images.update({_id: insertedFile}, {$set: {'recinto': result}});
-     }
-   }
- }
-});
-*/
+// AutoForm.hooks({
+//  uploadRecinto: {
+//    after: {
+//      insert: function(error, result, template) {
+//        insertedFile = Recintos.findOne(result).imagenes_id;
+//        Images.update({_id: insertedFile}, {$set: {'recinto': result}});
+//      }
+//    }
+//  }
+// });
+
 Template.uploadRecinto.helpers({
 
   imagenes:  function() {
