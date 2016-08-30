cd90851960d9755f857c234feb23d42384f8e8a7
Franco Catena <francocatena@gmail.com>
2016-08-30 11:01:27 -0300

    Datepickers con restricciones horarias

--- client/template/Organizar Partido/organizarPartido.js
+++ client/template/Organizar Partido/organizarPartido.js
@@ -9,15 +9,19 @@ Template.organizarPartido.onRendered(function () {
   this.$('#datetimepicker3').datetimepicker({
     locale: 'es',
     format:'H',
-    disabledTimeIntervals: [[moment({ h: 1 }), moment({ h: 9 })]],
-    showClear: true,
+    disabledHours: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    showClear: true
   });
 });
 
 Template.organizarPartido.events({
+  'click [data-picker-handle]': function (event) {
+    var datetimepicker = $(event.currentTarget).data('pickerHandle')
 
-          
+    $(datetimepicker).data('DateTimePicker').toggle()
+  }
 });
+
 Template.organizarPartido.helpers({
 	
   canchas: function () {
