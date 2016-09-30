43be336757198441ba3f50a14431e363cc67ea44
tchgc <tiincho.alvarez@gmail.com>
2016-09-30 17:38:09 -0300

    Se eliminaron rutas viejas,se re ordenaron rutas del dueño, se creo un boton de crear cancha en gestion cancha

--- client/template/owner/canchas/cargar_cancha_recinto.js
+++ client/template/owner/canchas/cargar_cancha_recinto.js
@@ -5,8 +5,9 @@ AutoForm.addHooks(
     insert: function (error, result) {
       if(! error)
         console.log(result);
+        var cancha = Canchas.findOne({_id: result})
         alert("Cancha creada correctamente");
-        
+        Router.go('gestionCancha',{_id:cancha.recintoId});
         
     }
   }
