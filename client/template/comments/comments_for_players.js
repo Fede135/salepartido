c76b6f4c02bed0428b6e9672d9f8bdb752d5bb8d
Federico Gomez <federico_135@hotmail.com>
2016-10-11 09:29:39 -0300

    se agrego filtro a comentarios

--- client/template/comments/comments_for_players.js
+++ client/template/comments/comments_for_players.js
@@ -1,6 +1,24 @@
 AutoForm.addHooks(
   ['enterCommentsPlayers'],
   {
+    before: {
+      insert: function(doc) {
+        console.log(doc);
+        var docFiltrado = doc.commentToPlayer.replace(/puto|pajero|cagón|culiado|pija|puta|concha|conchudo|conchuda|cagon/gi, function filtrar(x) {
+          var len= x.length;
+          var arr = []
+          for (var i = 0; i < len; i++) {
+            arr.push('*');
+          }
+          return arr.toString().replace(/,/g, "");
+        });
+        docModificado = {
+          commentToPlayer: docFiltrado,
+          toUserId: doc.toUserId,
+        };
+        return docModificado;
+       }
+    },
     after: {
       insert: function (error, result) {
         createCommentForPlayersNotification(result);
@@ -8,3 +26,4 @@ AutoForm.addHooks(
     } 
   },
 )
+
